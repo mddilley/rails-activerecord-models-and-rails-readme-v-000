3b8d7b41b41f80ea1151d7bc1aae0400078b3a73
Mike Dilley <mike.dilley@gmail.com>
2019-01-09 21:05:43 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_posts.rb
+++ db/migrate/001_create_posts.rb
@@ -3,8 +3,8 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.text :description
-
-      t.timestamp null: false
+ 
+      t.timestamps null: false
     end
   end
 end
